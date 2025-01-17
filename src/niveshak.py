dfb84012b0cddf3039c1191133d6be24d67b7c56
saurabhgarg-sg <20387189+saurabhgarg-sg@users.noreply.github.com>
2025-01-17 14:52:51 +0530

    gpg check

---
+++ src/niveshak.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+
+option = st.selectbox("Which number do you like best?", df["first column"])
+
+"You selected: ", option
