5bae75cec3e35a956cbbd3cf1f11eed3e1b270c1
saurabhgarg-sg <20387189+saurabhgarg-sg@users.noreply.github.com>
2025-01-17 19:01:19 +0530

    watchlist support on main page

--- src/niveshak.py
+++ src/niveshak.py
@@ -1,8 +1,28 @@
-import pandas as pd
+import os
+
 import streamlit as st
 
-df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+from constants.datafiles import DataFiles
 
-option = st.selectbox("Which number do you like best?", df["first column"])
 
-"You selected: ", option
+class Niveshak:
+    def get_watchlists(self) -> dict:
+        """Get all the watchlists."""
+        watchlists = {}
+        for entry in os.scandir(DataFiles.WATCHLISTS):
+            if entry.is_file():
+                watchlists[entry.name] = entry.path
+        return watchlists
+
+    def display_welcome_page(self):
+        """Show the main page to start the scanners."""
+        watchlists = self.get_watchlists()
+        option = st.selectbox(
+            "Which list to scan?",
+            sorted(watchlists.keys()),
+            index=None,
+            placeholder="select a list",
+        )
+
+
+Niveshak().display_welcome_page()
