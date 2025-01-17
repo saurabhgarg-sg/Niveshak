b74c766ed8e87e489b31dc6cd07c3bb0e13532b9
saurabhgarg-sg <20387189+saurabhgarg-sg@users.noreply.github.com>
2025-01-17 19:27:10 +0530

    minor refactoring

--- src/niveshak.py
+++ src/niveshak.py
@@ -1,20 +1,28 @@
+import logging
 import os
+import sys
+from pprint import pformat
 
 import streamlit as st
 
 from constants.datafiles import DataFiles
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
 
 class Niveshak:
     def get_watchlists(self) -> dict:
-        """Get all the watchlists."""
+        """Get all the watchlists.
+        :rtype: dict
+        """
         watchlists = {}
         for entry in os.scandir(DataFiles.WATCHLISTS):
             if entry.is_file():
                 watchlists[entry.name] = entry.path
+        logging.debug(pformat(watchlists))
         return watchlists
 
-    def display_welcome_page(self):
+    def display_welcome_page(self) -> None:
         """Show the main page to start the scanners."""
         watchlists = self.get_watchlists()
         option = st.selectbox(
